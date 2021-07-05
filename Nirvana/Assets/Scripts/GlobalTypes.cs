e6335b13f4b2c641d1a23db3d5cec865ffaf94e6
Jorne <mr.jorne@gmail.com>
2021-07-05 19:25:49 +0200

    Ground Enums

---
+++ Nirvana/Assets/Scripts/GlobalTypes.cs
@@ -0,0 +1,11 @@
+namespace GlobalTypes
+{
+    public enum GroundType
+    {
+        None,
+        OneWayPlatform,
+        MovingPlatform,
+        CollapseablePlatform,
+        JumpPad
+    }
+}
---
+++ Nirvana/Assets/Scripts/GroundEffector.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using GlobalTypes;
+using UnityEngine;
+
+public class GroundEffector : MonoBehaviour
+{
+    public GroundType GroundType;
+}
